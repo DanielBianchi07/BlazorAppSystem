3b36ac80e15752b636affdbd5413c7963b4fec1b
DanielBianchi07 <Daniel.Bianchi07@outlook.com>
2024-06-04 14:20:29 -0300

    Criação de treinamento handler

---
+++ BlazorApp.Api/Handlers/TreinamentoHandler.cs
@@ -0,0 +1,75 @@
+﻿using BlazorApp.Shared.Handlers;
+using BlazorApp.Shared.Models;
+using BlazorApp.Shared.Requests.Treinamentos;
+using BlazorApp.Shared.Responses;
+
+namespace BlazorApp.Api.Handlers
+{
+    public class TreinamentoHandler : ITreinamentoHandler
+    {
+        public Task<Response<Treinamento?>> CreateAsync(CreateTreinamentoRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<Treinamento?>> DeleteAsync(DeleteTreinamentoRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedResponse<List<Treinamento>?>> GetAllAsync(GetAllTreinamentosRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedResponse<List<Treinamento>?>> GetByDateAsync(GetTreinamentoByDateRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<Treinamento?>> GetByIdAsync(GetTreinamentoByIdRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedResponse<List<Treinamento>?>> GetTreinamentoByAlunoAsync(GetTreinamentoByAlunoRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedResponse<List<Treinamento>?>> GetTreinamentoByAlunoByDateAsync(GetTreinamentoByAlunoByDateRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedResponse<List<Treinamento>?>> GetTreinamentoByEmpresaAsync(GetTreinamentoByEmpresaRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedResponse<List<Treinamento>?>> GetTreinamentoByEmpresaByDateAsync(GetTreinamentoByEmpresaByDateRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedResponse<List<Treinamento>?>> GetTreinamentoByInstrutorAsync(GetTreinamentoByInstrutorRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedResponse<List<Treinamento>?>> GetTreinamentoByInstrutorByDateAsync(GetTreinamentoByInstrutorByDateRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedResponse<List<Treinamento>?>> GetTreinamentoBySitucaoAsync(GetTreinamentoBySituacaoRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<Treinamento?>> UpdateAsync(UpdateTreinamentoRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
